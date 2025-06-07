95e9b35a37e082b373d51351186751f1c0c30117
khushi kumari <khushik10201@gmail.com>
2025-06-07 22:45:21 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        training_sentences.append(pattern)
+        training_labels.append(intent['tag'])
+    responses.append(intent['responses'])
+
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
+
+number_of_classes = len(labels)
+
+print(number_of_classes)
